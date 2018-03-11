5825bac331cc778e46bd148cf0aa5ca8616a6113
Alexey Reznikov <oligsei.reznikov@gmail.com>
2018-03-11 19:47:26 +0200

    removed regex from search function

--- src/support/findContactsByCriteria.js
+++ src/support/findContactsByCriteria.js
@@ -1,2 +1,6 @@
-export const findContactsByCriteria = (contacts, criteria) => criteria === '' ? contacts : contacts
-    .filter(contact => new RegExp(criteria, 'ig').test(contact.firstName + contact.lastName));
+export const findContactsByCriteria = (contacts, criteria) => criteria === ''
+    ? contacts
+    : contacts.filter(contact => `${contact.firstName} ${contact.lastName}`
+        .toLowerCase()
+        .includes(criteria.toLowerCase())
+    );
